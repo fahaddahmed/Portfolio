61c3c8dcbf3d4b5ae7b78222fb6a7e1f34a9a8f7
Fahad Ahmed <fahadaahmed8@gmail.com>
2024-10-07 11:54:37 -0400

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -1,21 +1,28 @@
 export default [
     {
-      id: 1,
+        id: 1,
+        name: "Jedis",
+        desc: "A lightweight Redis memory database implemented in Java, with Maven handling the build automation for efficiency.",
+        imgSrc: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+        link: "https://github.com/fahaddahmed/Jedis",
+    }
+    {
+      id: 2,
       name: "Code Talk",
       desc: "A chatting app made for communication amongst Computer Science students.",
       imgSrc: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
       link: "https://github.com/fahaddahmed/CodeTalk",
     },
     {
-      id: 2,
-      name: "Quran Verse Bot",
-      desc: "Discord bot built with Node.js and Discord.js, that dynamically retrieves random Quranic verses and displays them alongside contextual information, ensuring reliability and user-friendliness through error handling and API integration.",
+      id: 3,
+      name: "Verse Bot",
+      desc: "Discord bot built with Node.js and Discord.js, that dynamically retrieves random philosophical excerpts and displays them alongside contextual information, ensuring reliability and user-friendliness through error handling and API integration.",
       imgSrc:
         "https://cdn-icons-png.flaticon.com/512/25/25231.png",
       link: "https://github.com/Muslims-in-Tech-MIT/QuranVerseBot",
     },
     {
-      id: 3,
+      id: 4,
       name: "Web Scraper",
       desc: "Simple web scraper made using Flask, Python, HTML, and Beautiful Soup. Designed to scrape for scholarships.",
       imgSrc:
@@ -23,7 +30,7 @@ export default [
       link: "https://github.com/fahaddahmed/Web_scraper",
     },
     {
-      id: 4,
+      id: 5,
       name: "DynamicAuto DB",
       desc: "Dynamic C-based application that efficiently manages car inventories using linked lists, featuring a user-friendly command-line interface for adding, editing, and querying car data.",
       imgSrc:

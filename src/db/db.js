ebadc13329e7bd2a3489fca506689b7fcfb8bbba
Fahad Ahmed <fahadaahmed8@gmail.com>
2024-11-11 22:53:18 -0500

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -1,20 +1,27 @@
 export default [
     {
         id: 1,
+        name: "Ktorus",
+        desc: "A Kotlin-powered HTTP server.",
+        imgSrc: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+        link: "https://github.com/fahaddahmed/Ktorus",
+    },
+    {
+        id: 2,
         name: "Jedis",
         desc: "A lightweight Redis memory database implemented in Java, with Maven handling the build automation for efficiency.",
         imgSrc: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
         link: "https://github.com/fahaddahmed/Jedis",
     },
     {
-      id: 2,
+      id: 3,
       name: "Code Talk",
       desc: "A chatting app made for communication amongst Computer Science students.",
       imgSrc: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
       link: "https://github.com/fahaddahmed/CodeTalk",
     },
     {
-      id: 3,
+      id: 4,
       name: "Verse Bot",
       desc: "Discord bot built with Node.js and Discord.js, that dynamically retrieves random philosophical excerpts and displays them alongside contextual information, ensuring reliability and user-friendliness through error handling and API integration.",
       imgSrc:
@@ -22,7 +29,7 @@ export default [
       link: "https://github.com/Muslims-in-Tech-MIT/QuranVerseBot",
     },
     {
-      id: 4,
+      id: 5,
       name: "Web Scraper",
       desc: "Simple web scraper made using Flask, Python, HTML, and Beautiful Soup. Designed to scrape for scholarships.",
       imgSrc:
@@ -30,7 +37,7 @@ export default [
       link: "https://github.com/fahaddahmed/Web_scraper",
     },
     {
-      id: 5,
+      id: 6,
       name: "Inventory Management Software",
       desc: "Dynamic C-based application that efficiently manages car inventories using linked lists, featuring a user-friendly command-line interface for adding, editing, and querying car data.",
       imgSrc:

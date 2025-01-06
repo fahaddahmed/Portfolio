d5c304a18201f8b6ca3a7756fdbda03665772c01
Fahad Ahmed <fahadaahmed8@gmail.com>
2025-01-06 16:41:14 -0500

    Update experience.js

--- src/db/experience.js
+++ src/db/experience.js
@@ -1,12 +1,18 @@
 export default [
   {
     id: 1,
+    companyName: "University of Guelph",
+    desc: "Undergraduate Teaching Assistant",
+    duration: "January 2025 – Present",
+  },
+  {
+    id: 2,
     companyName: "Teachify",
     desc: "Software Developer Intern",
     duration: "January 2024 – May 2024",
   },
   {
-    id: 2,
+    id: 3,
     companyName: "Milton District High School Programming Club",
     desc: "Assistant Lead ",
     duration: "October 2022 – April 2023",

d5a5d2833ee4d3abec0a088a6d8b3c3dcd030715
fahaddahmed <fahadaahmed8@gmail.com>
2025-01-09 21:04:57 -0500

    Fixed github logo colouring

--- src/db/db.js
+++ src/db/db.js
@@ -3,21 +3,21 @@ export default [
         id: 1,
         name: "Ktorus",
         desc: "A Kotlin-powered HTTP server.",
-        imgSrc: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+        imgSrc: "src/assets/github-logo.png",
         link: "https://github.com/fahaddahmed/Ktorus",
     },
     {
         id: 2,
         name: "Jedis",
         desc: "A lightweight Redis memory database implemented in Java, with Maven handling the build automation for efficiency.",
-        imgSrc: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+        imgSrc: "src/assets/github-logo.png",
         link: "https://github.com/fahaddahmed/Jedis",
     },
     {
       id: 3,
       name: "Code Talk",
       desc: "A chatting app made for communication amongst Computer Science students.",
-      imgSrc: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+      imgSrc: "src/assets/github-logo.png",
       link: "https://github.com/fahaddahmed/CodeTalk",
     },
     {
@@ -25,7 +25,7 @@ export default [
       name: "Verse Bot",
       desc: "Discord bot built with Node.js and Discord.js, that dynamically retrieves random philosophical excerpts and displays them alongside contextual information, ensuring reliability and user-friendliness through error handling and API integration.",
       imgSrc:
-        "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+        "src/assets/github-logo.png",
       link: "https://github.com/Muslims-in-Tech-MIT/QuranVerseBot",
     },
     {
@@ -33,7 +33,7 @@ export default [
       name: "Web Scraper",
       desc: "Simple web scraper made using Flask, Python, HTML, and Beautiful Soup. Designed to scrape for scholarships.",
       imgSrc:
-        "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+        "src/assets/github-logo.png",
       link: "https://github.com/fahaddahmed/Web_scraper",
     },
     {
@@ -41,7 +41,7 @@ export default [
       name: "Inventory Management Software",
       desc: "Dynamic C-based application that efficiently manages car inventories using linked lists, featuring a user-friendly command-line interface for adding, editing, and querying car data.",
       imgSrc:
-        "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+        "src/assets/github-logo.png",
       link: "https://github.com/fahaddahmed/Inventory-Management-Software",
     },
   ];

133de9e59e526f7aca20883df5ac50f57448e3aa
fahaddahmed <fahadaahmed8@gmail.com>
2025-01-11 18:41:29 -0500

    Fix spelling

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -22,7 +22,7 @@ const Intro = () => {
       {/* about yourself */}
       <div className="desc">
         <p>
-        "Hi! I am Fahad Ahmed, a <span>second-year</span> Computer Science student at the University of Guelph. 
+        Hi! I am Fahad Ahmed, a <span>second-year</span> Computer Science student at the University of Guelph. 
         My interest in technology began in high school when I started coding. Working on my first project 
         got me interested in programming since then, and I joined different clubs and hackathons to push my 
         skills further!
@@ -30,10 +30,8 @@ const Intro = () => {
         <p>
         I love how computer science allows you to build a broad variety of passions. It's a versatile degree, 
         which allows me to not only learn about coding but also think of ways I can use my skills to build things 
-        I'm interested in! Coding is not just a skill for me, but a way to bring concepts to life. 
-        </p>
-        <p>
-        As I continue my studies, I plan to learn even more about computer science and other subjects. I hope 
+        I'm interested in! Coding is not just a skill for me, but a way to bring concepts to life. As I continue my 
+        studies, I plan to learn even more about computer science and other subjects. I hope 
         to keep growing from every challenge and explore all the possibilities ahead. 
         </p>
         <p>
